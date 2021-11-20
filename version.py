d8c9e6da52ab99d8eab752690ff02f980cba4837
banesullivan <banesullivan@gmail.com>
2021-11-20 14:34:19 -0700

    Bump version

--- version.py
+++ version.py
@@ -15,7 +15,7 @@ Which denotes the first release candidate.
 
 """
 # major, minor, patch
-version_info = 0, 0, 1
+version_info = 0, 0, 2
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
