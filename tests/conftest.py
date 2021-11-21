63d064980d51fa223e4f4ae9256434a3d9142759
banesullivan <banesullivan@gmail.com>
2021-11-21 16:03:11 -0700

    Add test for two concurrent servers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tileserver.examples import get_data_path, get_bahamas
+from tileserver.examples import get_data_path, get_bahamas, get_blue_marble
 
 
 @pytest.fixture
@@ -9,6 +9,12 @@ def bahamas_file():
 
 
 @pytest.fixture
-def bahamas_tile_server(port=0, debug=True):
+def bahamas(port=0, debug=True):
     # Using debug True since in a testing environment
     return get_bahamas(port=port, debug=debug)
+
+
+@pytest.fixture
+def blue_marble(port=0, debug=True):
+    # Using debug True since in a testing environment
+    return get_blue_marble(port=port, debug=debug)
--- tests/test_tile_server.py
+++ tests/test_tile_server.py
@@ -7,6 +7,12 @@ from tileserver.application.paths import _THREAD_FILE_PATHS
 TOLERANCE = 2e-2
 
 
+def get_content(url):
+    r = requests.get(url)
+    r.raise_for_status()
+    return r.content
+
+
 def test_create_tile_server(bahamas_file):
     tile_server = TileServer(bahamas_file, debug=True)
     assert tile_server.path == bahamas_file
@@ -23,27 +29,37 @@ def test_create_tile_server(bahamas_file):
     assert r.content
 
 
-def test_server_shutdown(bahamas_tile_server):
-    tile_url = bahamas_tile_server.get_tile_url().format(z=8, x=72, y=110)
+def test_server_shutdown(bahamas):
+    tile_url = bahamas.get_tile_url().format(z=8, x=72, y=110)
     r = requests.get(tile_url)
     r.raise_for_status()
     assert r.content
     assert len(_THREAD_FILE_PATHS) == 1
-    bahamas_tile_server.shutdown()
+    bahamas.shutdown()
     assert len(_THREAD_FILE_PATHS) == 0
     with pytest.raises(requests.ConnectionError):
         r = requests.get(tile_url)
         r.raise_for_status()
 
 
-def test_server_delete(bahamas_tile_server):
-    tile_url = bahamas_tile_server.get_tile_url().format(z=8, x=72, y=110)
+def test_server_delete(bahamas):
+    tile_url = bahamas.get_tile_url().format(z=8, x=72, y=110)
     r = requests.get(tile_url)
     r.raise_for_status()
     assert r.content
     assert len(_THREAD_FILE_PATHS) == 1
-    bahamas_tile_server.__del__()  # Using `del` does not always work
+    bahamas.__del__()  # Using `del` does not always work
     assert len(_THREAD_FILE_PATHS) == 0
     with pytest.raises(requests.ConnectionError):
         r = requests.get(tile_url)
         r.raise_for_status()
+
+
+def test_multiple_tile_servers(bahamas, blue_marble):
+    assert bahamas.port != blue_marble.port
+    tile_url_a = bahamas.get_tile_url().format(z=8, x=72, y=110)
+    tile_url_b = blue_marble.get_tile_url().format(z=8, x=72, y=110)
+    assert get_content(tile_url_a) != get_content(tile_url_b)
+    thumb_url_a = bahamas.create_url("thumbnail")
+    thumb_url_b = blue_marble.create_url("thumbnail")
+    assert get_content(thumb_url_a) != get_content(thumb_url_b)
