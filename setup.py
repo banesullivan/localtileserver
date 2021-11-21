1360bc3d7550a901895e915fc92a9b2c7717242e
banesullivan <banesullivan@gmail.com>
2021-11-21 16:43:35 -0700

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
     long_description = ""
 
 # major, minor, patch
-version_info = 0, 1, "dev0"
+version_info = 0, 1, 0
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
 
