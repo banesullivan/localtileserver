b52bf726fa6ea251752b2c2c22aec31f972b42fe
banesullivan <banesulli@gmail.com>
2021-11-26 18:51:32 -0700

    Linting routines!

--- localtileserver/__init__.py
+++ localtileserver/__init__.py
@@ -4,4 +4,4 @@ from localtileserver.server import TileClient, get_or_create_tile_client  # noqa
 from localtileserver.utilities import get_cache_dir  # noqa
 from localtileserver.widgets import get_folium_tile_layer  # noqa
 from localtileserver.widgets import get_leaflet_roi_controls  # noqa
-from localtileserver.widgets import get_leaflet_tile_layer
+from localtileserver.widgets import get_leaflet_tile_layer  # noqa
