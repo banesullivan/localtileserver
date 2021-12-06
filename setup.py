0c444034f63165857c57f4043a05cb73b5df5b71
banesullivan <banesulli@gmail.com>
2021-12-05 23:03:17 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
     long_description = ""
 
 # major, minor, patch
-version_info = 0, 3, 5
+version_info = 0, 3, 6
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
 
