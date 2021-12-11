157b68b1c3591e31162a45dd9eb0e49f9f12a08f
banesullivan <banesulli@gmail.com>
2021-12-11 02:22:13 -0700

    Attempt fix heroku deploy

--- localtileserver/tileserver/wsgi.py
+++ localtileserver/tileserver/wsgi.py
@@ -2,12 +2,9 @@ import logging
 
 from localtileserver.tileserver import create_app
 
-if __name__ == "__main__":
+logging.getLogger("werkzeug").setLevel(logging.DEBUG)
+logging.getLogger("gdal").setLevel(logging.DEBUG)
+logging.getLogger("large_image").setLevel(logging.DEBUG)
+logging.getLogger("large_image_source_gdal").setLevel(logging.DEBUG)
 
-    logging.getLogger("werkzeug").setLevel(logging.DEBUG)
-    logging.getLogger("gdal").setLevel(logging.DEBUG)
-    logging.getLogger("large_image").setLevel(logging.DEBUG)
-    logging.getLogger("large_image_source_gdal").setLevel(logging.DEBUG)
-
-    app = create_app()
-    app.run()
+app = create_app()
