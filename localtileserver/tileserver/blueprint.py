7cc51d3ea720eb150892fed4d33a50bd8721cd45
banesullivan <banesulli@gmail.com>
2021-12-12 15:06:28 -0700

    Fix flask cache init

--- localtileserver/tileserver/blueprint.py
+++ localtileserver/tileserver/blueprint.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from flask_caching import Cache, MemcachedCache
+
+from localtileserver.tileserver.utilities import get_memcache_config
 
 tileserver = Blueprint(
     "tileserver",
@@ -9,13 +10,26 @@ tileserver = Blueprint(
     template_folder="templates",
 )
 
+
 def create_cache(url: str, username: str = None, password: str = None):
-    if url:
-        config = {"CACHE_MEMCACHED_SERVERS": url}
-        if username and password:
-            config["CACHE_MEMCACHED_USERNAME"] = username
-            config["CACHE_MEMCACHED_PASSWORD"] = password
-        cache = MemcachedCache(config=config)
-    else:
+    try:
+        from flask_caching import MemcachedCache, SASLMemcachedCache
+
+        if url:
+            config = {"CACHE_MEMCACHED_SERVERS": url}
+            if username and password:
+                config["CACHE_MEMCACHED_USERNAME"] = username
+                config["CACHE_MEMCACHED_PASSWORD"] = password
+                cache = SASLMemcachedCache(config=config)
+            else:
+                cache = MemcachedCache(config=config)
+        else:
+            raise ImportError
+    except ImportError:
+        from flask_caching import Cache
+
         cache = Cache(config={"CACHE_TYPE": "SimpleCache"})
     return cache
+
+
+cache = create_cache(*get_memcache_config())
