9b3fb63912473138372eeff28d82943e7ec8da06
banesullivan <banesulli@gmail.com>
2021-12-12 15:01:15 -0700

    Add pylibmc to depends

--- localtileserver/tileserver/blueprint.py
+++ localtileserver/tileserver/blueprint.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask_caching import Cache
+from flask_caching import Cache, MemcachedCache
 
 tileserver = Blueprint(
     "tileserver",
@@ -8,4 +8,14 @@ tileserver = Blueprint(
     static_url_path="/static/tileserver",
     template_folder="templates",
 )
-cache = Cache(config={"CACHE_TYPE": "SimpleCache"})
+
+def create_cache(url: str, username: str = None, password: str = None):
+    if url:
+        config = {"CACHE_MEMCACHED_SERVERS": url}
+        if username and password:
+            config["CACHE_MEMCACHED_USERNAME"] = username
+            config["CACHE_MEMCACHED_PASSWORD"] = password
+        cache = MemcachedCache(config=config)
+    else:
+        cache = Cache(config={"CACHE_TYPE": "SimpleCache"})
+    return cache
