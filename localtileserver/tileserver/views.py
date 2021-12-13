69e24bec4fa871ae59bd2a8f6bfd7b5a99533131
banesullivan <banesulli@gmail.com>
2021-12-12 20:46:41 -0700

    Handle bad files with 404 page

--- localtileserver/tileserver/views.py
+++ localtileserver/tileserver/views.py
@@ -2,6 +2,7 @@ import logging
 
 from flask import current_app, render_template, request
 from flask.views import View
+from large_image.exceptions import TileSourceFileNotFoundError
 
 from localtileserver.tileserver import utilities
 from localtileserver.tileserver.blueprint import tileserver
@@ -10,18 +11,7 @@ from localtileserver.tileserver.data import get_data_path
 logger = logging.getLogger(__name__)
 
 
-class GeoJSViewer(View):
-    def dispatch_request(self):
-        return render_template("tileserver/geojsViewer.html")
-
-
-class CesiumViewer(View):
-    def dispatch_request(self):
-        return render_template("tileserver/cesiumViewer.html")
-
-
-@tileserver.context_processor
-def inject_context():
+def _get_clean_filename_from_request():
     try:
         # First look for filename in URL params
         f = request.args.get("filename")
@@ -36,11 +26,52 @@ def inject_context():
             # Fallback to sample data
             logger.error("No filename set in app config or URL params. Using sample data.")
             filename = get_data_path("landsat.tif")
-    tile_source = utilities.get_tile_source(filename)
-    context = utilities.get_meta_data(tile_source)
-    context["bounds"] = utilities.get_tile_bounds(tile_source, projection="EPSG:4326")
+    return filename
+
+
+class BaseViewer(View):
+    def render_or_404(self, template: str):
+        """Check the file in the arguments and 404 if invalid."""
+        projection = request.args.get("projection", None)
+        # Check opening the file with large image
+        try:
+            filename = _get_clean_filename_from_request()
+            _ = utilities.get_tile_source(filename, projection=projection)
+        except (OSError, AttributeError, TileSourceFileNotFoundError):
+            return render_template("tileserver/404file.html"), 404
+        return render_template(template)
+
+
+class GeoJSViewer(BaseViewer):
+    def dispatch_request(self):
+        return self.render_or_404("tileserver/geojsViewer.html")
+
+
+class CesiumViewer(BaseViewer):
+    def dispatch_request(self):
+        return self.render_or_404("tileserver/cesiumViewer.html")
+
+
+@tileserver.context_processor
+def raster_context():
+    try:
+        filename = _get_clean_filename_from_request()
+    except OSError:
+        filename = request.args.get("filename", "")
+    context = {}
     context["filename"] = filename
-    # Sample data
+    try:
+        tile_source = utilities.get_tile_source(filename)
+    except (OSError, AttributeError, TileSourceFileNotFoundError):
+        return context
+    context.update(utilities.get_meta_data(tile_source))
+    context["bounds"] = utilities.get_tile_bounds(tile_source, projection="EPSG:4326")
+    return context
+
+
+@tileserver.context_processor
+def sample_data_context():
+    context = {}
     context["filename_dem"] = get_data_path("aws_elevation_tiles_prod.xml")
     context["filename_bluemarble"] = get_data_path("frmt_wms_bluemarble_s3_tms.xml")
     context["filename_virtualearth"] = get_data_path("frmt_wms_virtualearth.xml")
