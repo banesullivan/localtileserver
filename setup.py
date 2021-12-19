bd2653f1c31fc389c8e77e6e87b0e376e67a97fc
banesullivan <banesulli@gmail.com>
2021-12-19 15:08:07 -0700

    Limit flask-restx depend

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         "click",
         "flask>=2.0.0",
         "Flask-Caching",
-        "flask-restx",
+        "flask-restx>=0.5.0",
         "GDAL",
         "large_image",
         "large_image_source_gdal",
