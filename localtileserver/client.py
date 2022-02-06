2059db5ead118105eba8fbd57e3ddaae73d222e5
banesullivan <banesulli@gmail.com>
2022-02-06 12:25:03 -0700

    Linting

--- localtileserver/client.py
+++ localtileserver/client.py
@@ -395,7 +395,7 @@ class TileClient(BaseTileClient):
         elif self.client_port is None and self.client_host is not None:
             base = f"http://{self.client_host}"
         else:
-            base = ''  # Use relative path
+            base = ""  # Use relative path
         if self.client_prefix is not None:
             return f"{base}/{self.client_prefix}"
         return base
