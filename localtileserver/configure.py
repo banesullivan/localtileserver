1fa441b9260dfa83e01c4e99b2c97d4a3a06c644
banesullivan <banesulli@gmail.com>
2022-02-05 21:51:25 -0700

    Better handle unset variables

--- localtileserver/configure.py
+++ localtileserver/configure.py
@@ -9,10 +9,22 @@ def get_default_port():
 
 
 def get_default_client_params(host: str = None, port: int = None, prefix: str = None):
-    if host is None and "LOCALTILESERVER_CLIENT_HOST" in os.environ:
+    if (
+        host is None
+        and "LOCALTILESERVER_CLIENT_HOST" in os.environ
+        and os.environ["LOCALTILESERVER_CLIENT_HOST"]
+    ):
         host = str(os.environ["LOCALTILESERVER_CLIENT_HOST"])
-    if port is None and "LOCALTILESERVER_CLIENT_PORT" in os.environ:
+    if (
+        port is None
+        and "LOCALTILESERVER_CLIENT_PORT" in os.environ
+        and os.environ["LOCALTILESERVER_CLIENT_PORT"]
+    ):
         port = int(os.environ["LOCALTILESERVER_CLIENT_PORT"])
-    if prefix is None and "LOCALTILESERVER_CLIENT_PREFIX" in os.environ:
+    if (
+        prefix is None
+        and "LOCALTILESERVER_CLIENT_PREFIX" in os.environ
+        and os.environ["LOCALTILESERVER_CLIENT_PREFIX"]
+    ):
         prefix = str(os.environ["LOCALTILESERVER_CLIENT_PREFIX"])
     return host, port, prefix
