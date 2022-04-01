2183998904e3a5aee2cbfb95a27d8061e42e1c67
banesullivan <banesulli@gmail.com>
2022-04-01 16:25:07 -0600

    Use sentry

---
+++ localtileserver/tileserver/sentry.py
@@ -0,0 +1,16 @@
+import os
+
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_dsn = os.environ.get("SENTRY_DSN", "")
+
+if sentry_dsn:
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+        integrations=[FlaskIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+    )
