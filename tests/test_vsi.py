ec392871182cb62775b18f4e737c75503b369237
banesullivan <banesulli@gmail.com>
2022-04-16 16:36:42 -0600

    Disable S3 test

--- tests/test_vsi.py
+++ tests/test_vsi.py
@@ -1,3 +1,5 @@
+import pytest
+
 from localtileserver.client import TileClient
 
 
@@ -6,6 +8,7 @@ def test_tileclient_with_vsi(remote_file_url):
     assert "bounds" in tile_client.metadata()
 
 
+@pytest.mark.skip
 def test_tileclient_with_vsi_s3(remote_file_s3):
     tile_client = TileClient(remote_file_s3)
     assert "bounds" in tile_client.metadata()
