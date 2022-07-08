32a439882a806f78beb9131d40f66278b08fb2f4
Bane Sullivan <banesulli@gmail.com>
2022-07-08 08:21:32 -0600

    Add jupyterlab to report

--- localtileserver/report.py
+++ localtileserver/report.py
@@ -35,6 +35,7 @@ class Report(scooby.Report):
             "gunicorn",
             "pylibmc",
             "ipyleaflet",
+            "jupyterlab",
             "traitlets",
             "shapely",
             "folium",
