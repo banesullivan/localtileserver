2adcb266dcf0e82d01726dee3d2198784fe481df
Bane Sullivan <banesulli@gmail.com>
2022-07-24 23:22:53 -0700

    add LocalTileServerLayerMixin class (#105)
    * add LocalTileServerLayerMixin class
    
    * linting

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         "requests",
         "server-thread",
         "scooby",
-        "werkzeug<2.2"
+        "werkzeug<2.2",
     ],
     extras_require={
         "colormaps": ["matplotlib", "colorcet", "cmocean"],
