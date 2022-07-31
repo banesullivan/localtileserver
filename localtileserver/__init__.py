c957994e9f48a6ae6fd20b8ae31683d6c7bf26c5
banesullivan <banesulli@gmail.com>
2022-07-30 19:06:51 -0600

    Add validate_cog to imports

--- localtileserver/__init__.py
+++ localtileserver/__init__.py
@@ -3,6 +3,7 @@ from localtileserver._version import __version__
 from localtileserver.client import RemoteTileClient, TileClient, get_or_create_tile_client
 from localtileserver.report import Report
 from localtileserver.tileserver.utilities import get_cache_dir, make_vsi, purge_cache
+from localtileserver.validate import validate_cog
 from localtileserver.widgets import (
     LocalTileServerLayerMixin,
     get_folium_tile_layer,
