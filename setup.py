5a5ee4627e47da2be9d2b28183a850722c1308cf
Bane Sullivan <banesullivan@gmail.com>
2022-11-02 16:40:27 -0600

    Stop testing on 3.7

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python",
     ],
     python_requires=">=3.7",
