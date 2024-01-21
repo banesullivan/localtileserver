bbd49b0f70116377a4df3f1a82f9f7b801df9439
banesullivan <banesullivan@users.noreply.github.com>
2024-01-21 23:58:37 +0000

    deploy: 9604b8a4f1eb23e760b6dae89ac68bca84ba6d41

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.10.0.dev0',
+    VERSION: '0.10.0rc1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
