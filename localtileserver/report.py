46fa22ad9f97523245c8bbfd5d49b8e33649f04a
Bane Sullivan <banesullivan@gmail.com>
2024-01-21 19:02:06 -0800

    Switch to pyproject.toml

--- localtileserver/report.py
+++ localtileserver/report.py
@@ -6,38 +6,37 @@ class Report(scooby.Report):
         """Generate a report on the dependencies of localtileserver in this environment."""
 
         # Mandatory packages.
-        rio_tiler_core = [
-            "rasterio",
-            "rio_tiler",
-            "numpy",
-        ]
-        core = [
-            "localtileserver",
-            "flask",
-            "flask_caching",
-            "flask_cors",
-            "flask_restx",
-            "requests",
-            "rio_cogeo",
-            "werkzeug",
-            "click",
-            "server_thread",
-            "scooby",
-        ] + rio_tiler_core
+        core = ["localtileserver"] + sorted(
+            [
+                "rasterio",
+                "rio_tiler",
+                "numpy",
+                "server_thread",
+                "flask",
+                "flask_caching",
+                "flask_cors",
+                "flask_restx",
+                "rio_cogeo",
+                "werkzeug",
+                "click",
+            ]
+        )
 
         # Optional packages.
-        optional = [
-            "gunicorn",
-            "ipyleaflet",
-            "jupyterlab",
-            "jupyter_server_proxy",
-            "traitlets",
-            "shapely",
-            "folium",
-            "matplotlib",
-            "colorcet",
-            "cmocean",
-        ]
+        optional = sorted(
+            [
+                "gunicorn",
+                "ipyleaflet",
+                "jupyterlab",
+                "jupyter_server_proxy",
+                "traitlets",
+                "shapely",
+                "folium",
+                "matplotlib",
+                "requests" "colorcet",
+                "cmocean",
+            ]
+        )
 
         scooby.Report.__init__(
             self,
