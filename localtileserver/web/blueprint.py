c1e7bfdded3fccce25ac8bdb660c7854b46f4046
Bane Sullivan <banesullivan@gmail.com>
2024-02-11 15:42:33 -0800

    Remove caching on webserver

--- localtileserver/web/blueprint.py
+++ localtileserver/web/blueprint.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Blueprint
 from flask_caching import Cache
 
@@ -12,34 +10,4 @@ tileserver = Blueprint(
 )
 
 
-def get_memcache_config():
-    url, username, password = None, None, None
-    if os.environ.get("MEMCACHED_URL", ""):
-        url = os.environ.get("MEMCACHED_URL")
-        if os.environ.get("MEMCACHED_USERNAME", "") and os.environ.get("MEMCACHED_PASSWORD", ""):
-            username = os.environ.get("MEMCACHED_USERNAME")
-            password = os.environ.get("MEMCACHED_PASSWORD")
-    elif os.environ.get("MEMCACHIER_SERVERS", ""):
-        url = os.environ.get("MEMCACHIER_SERVERS")
-        if os.environ.get("MEMCACHIER_USERNAME", "") and os.environ.get("MEMCACHIER_PASSWORD", ""):
-            username = os.environ.get("MEMCACHIER_USERNAME")
-            password = os.environ.get("MEMCACHIER_PASSWORD")
-    return url, username, password
-
-
-def create_cache(url: str = None, username: str = None, password: str = None):
-    if url:
-        config = {"CACHE_MEMCACHED_SERVERS": url.split(",")}
-        if username and password:
-            config["CACHE_TYPE"] = "SASLMemcachedCache"
-            config["CACHE_MEMCACHED_USERNAME"] = username
-            config["CACHE_MEMCACHED_PASSWORD"] = password
-        else:
-            config["CACHE_TYPE"] = "MemcachedCache"
-
-    else:
-        config = {"CACHE_TYPE": "SimpleCache"}
-    return Cache(config=config)
-
-
-cache = create_cache(*get_memcache_config())
+cache = Cache(config={"CACHE_TYPE": "SimpleCache"})
