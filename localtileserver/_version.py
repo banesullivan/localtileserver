85014efac9b48bf05da439f4cd4f6e905d145dfd
Bane Sullivan <banesullivan@gmail.com>
2025-12-03 10:30:02 -0800

    Maintenance (#244)
    * Update GitHub Actions
    
    * Deprecate Py3.8 and Py3.9
    
    * Update pre-commit
    
    * Update precommit flake8
    
    * Use importlib over pkg_resources
    
    * Upgrade Python in action
    
    * Do not stash coverage
    
    * Fix python version
    
    * Fix srtm90.tif

--- localtileserver/_version.py
+++ localtileserver/_version.py
@@ -1,7 +1,7 @@
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = get_distribution("localtileserver").version
-except DistributionNotFound:  # pragma: no cover
+    __version__ = version("localtileserver")
+except PackageNotFoundError:  # pragma: no cover
     # package is not installed
     __version__ = None
