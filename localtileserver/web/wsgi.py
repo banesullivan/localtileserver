ec387f90968d8a18be9326a7393e94751ba5bbac
Bane Sullivan <banesullivan@gmail.com>
2026-02-20 22:55:30 -0800

    Spin up server for development docs

--- localtileserver/web/wsgi.py
+++ localtileserver/web/wsgi.py
@@ -4,6 +4,10 @@ ASGI application entry point.
 Used by uvicorn (e.g., ``uvicorn localtileserver.web.wsgi:app``).
 """
 
+import os
+
+from localtileserver.tiler.data import str_to_bool
 from localtileserver.web import create_app
 
-app = create_app()
+cors_all = str_to_bool(os.environ.get("LOCALTILESERVER_CORS_ALL", "false"))
+app = create_app(cors_all=cors_all)
